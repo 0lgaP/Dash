617899fd9e011667739651eba17bf4c5ceb8bfa6
0lgatron <perronolga@gmail.com>
2023-04-28 16:57:21 -0400

    icon update

---
+++ tmp/FirebaseCourse/olganization/Process.md
@@ -0,0 +1,26 @@
+>1
+```
+npm create vite@latest my-vue-app
+```
+* npm run dev
+* npm run build
+
+
+>2
+```
+https://console.firebase.google.com
++ create project
++ add web app -> register app
++ Project Overview -> app -> settings wheel ->  scroll down for config
+
+Enable services on the backend
+> Firestore Database
+> Authentication
+> etc..
+
+```
+>3
+```
+npm install firebase
+```
+>4
--- tmp/FirebaseCourse/olganization/src/App.jsx
+++ tmp/FirebaseCourse/olganization/src/App.jsx
@@ -1,13 +1,21 @@
 import './App.css'
+import activity from './assets/activity_icon.svg'
+import home from './assets/temple.svg'
+import dash from './assets/dashboard_icon.svg'
+import add from './assets/add_icon.svg'
 
 function App() {
 
 
   return (
     <>
-      
+      <img src={home}></img>
+      <img src={add}></img>
+      <img src={activity}></img>
+      <img src={dash}></img>
     </>
   )
 }
 
 export default App
+
