1bad7c509dfbc65c53ef4e18538d6d791c568493
0lgatron <perronolga@gmail.com>
2023-05-08 16:07:54 -0400

    add toggle-online users

--- tmp/FirebaseCourse/olganization/src/components/sidebar/OnlineUsers.jsx
+++ tmp/FirebaseCourse/olganization/src/components/sidebar/OnlineUsers.jsx
@@ -1,12 +1,36 @@
+import {useState} from 'react'
 import {useCollection} from "../../hooks/useCollection"
 import Avatar from "../avatar/Avatar"
 import "./OnlineUsers.css"
 
 const OnlineUsers = () => {
+  const [open, setOpen] = useState(true)
   const {doc, error} = useCollection('users')
+
+  const toggleOpen = () => setOpen(prev => !prev)
+  if(!open) {
+    return (
+      <div className="user-list collapsed">
+        <div className="toggle-container">
+      <button className='btn-toggle' aria-label="hide names of team members" aria-pressed={!open} onClick={toggleOpen}> {"+"} </button>
+
+      </div>
+      {doc && doc.map((user) => (
+        <div key={user.id} className="user-list-item">
+          <div>
+          {user.online && <span className="online-user"></span>}
+          </div>
+          <Avatar src={user.photoURL} name={user.displayName}/>
+        </div>
+      ))}
+   
+    </div>
+    )
+  }
   return (
     <div className="user-list">
-      <h2>All Users</h2>
+      <div className="toggle-container">
+      <button className='btn-toggle' onClick={toggleOpen} aria-label="hide names of team members" aria-pressed={!open}> {"-"} </button><h2>Online</h2></div>
       {error && <div className="error">{error}</div>}
       {doc && doc.map((user) => (
         <div key={user.id} className="user-list-item">
