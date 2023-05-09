86445ab66b12ebc500541666bb6e2eea6b4edd4a
0lgatron <perronolga@gmail.com>
2023-05-09 10:43:50 -0400

    add filter logic, add psedocode for rules

--- tmp/FirebaseCourse/olganization/src/pages/dashboard/Dashboard.jsx
+++ tmp/FirebaseCourse/olganization/src/pages/dashboard/Dashboard.jsx
@@ -1,6 +1,6 @@
 import { useCollection } from "../../hooks/useCollection";
 import { useState } from "react";
-
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 // styles
 import "./Dashboard.css";
@@ -9,26 +9,57 @@ import List from "../../components/list/List";
 import ProjectFilter from "./ProjectFilter";
 
 // hardcoded vals
-const filtersList = ["all", "mine", "development", "design", "marketing", "sales"];
-
+const filtersList = [
+  "all",
+  "mine",
+  "development",
+  "design",
+  "marketing",
+  "sales",
+];
 
 const Dashboard = () => {
   const [currentFilter, setCurrentFilter] = useState("all");
   const { doc, error } = useCollection("projects");
-  console.log("dash",doc)
+  const { user } = useAuthContext();
 
   const handleFilter = (newFilter) => {
-    console.log(newFilter);
     setCurrentFilter(newFilter);
   };
+
+  const filteredList =
+    doc &&
+    doc.filter((each) => {
+      switch (currentFilter) {
+        case "all":
+          return true;
+        case "mine": {
+          let assignedToMe = false;
+          each.assignedUsersList.forEach((u) => {
+            if (user.uid === u.id) {
+              assignedToMe = true;
+            }
+          });
+          return assignedToMe;
+        }
+        default: {
+          return each.category === currentFilter;
+        }
+      }
+    });
 
   return (
     <div>
       <h2 className="page-title">Dashboard</h2>
-
       {error && <p className="error">{error}</p>}
-      {doc && <ProjectFilter onClick={handleFilter} filtersList={filtersList} currentFilter={currentFilter}/>}
-      {doc && <List projects={doc} />}
+      {doc && (
+        <ProjectFilter
+          onClick={handleFilter}
+          filtersList={filtersList}
+          currentFilter={currentFilter}
+        />
+      )}
+      {filteredList && <List projects={filteredList} />}
     </div>
   );
 };
